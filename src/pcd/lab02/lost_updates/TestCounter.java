d1eb68f56e52b2bcbed14ad0d892bab0c1f7338c
Ledmington <68538713+Ledmington@users.noreply.github.com>
2022-03-07 14:44:07 +0100

    Changed Worker to use an explicit mutex

--- src/pcd/lab02/lost_updates/TestCounter.java
+++ src/pcd/lab02/lost_updates/TestCounter.java
@@ -1,13 +1,16 @@
 package pcd.lab02.lost_updates;
 
+import java.util.concurrent.locks.*;
+
 public class TestCounter {
 
 	public static void main(String[] args) throws Exception {
 		//int ntimes = Integer.parseInt(args[0]);
 		int ntimes = 100000;
+		Lock lock = new ReentrantLock();
 		UnsafeCounter c = new UnsafeCounter(0);
-		Worker w1 = new Worker(c, ntimes);
-		Worker w2 = new Worker(c, ntimes);
+		Worker w1 = new Worker(c, ntimes, lock);
+		Worker w2 = new Worker(c, ntimes, lock);
 
 		Cron cron = new Cron();
 		cron.start();
--- src/pcd/lab02/lost_updates/Worker.java
+++ src/pcd/lab02/lost_updates/Worker.java
@@ -1,19 +1,30 @@
 package pcd.lab02.lost_updates;
 
+import java.util.concurrent.locks.*;
 
 public class Worker extends Thread {
 	
-	private UnsafeCounter counter;
-	private int ntimes;
+	private final UnsafeCounter counter;
+	private final int ntimes;
+	private final Lock lock;
 	
-	public Worker(UnsafeCounter c, int ntimes){
+	public Worker(UnsafeCounter c, int ntimes, Lock lock) {
 		counter = c;
 		this.ntimes = ntimes;
+		this.lock = lock;
 	}
 	
-	public void run(){
-		for (int i = 0; i < ntimes; i++){
-			counter.inc();
+	public void run() {
+		for (int i = 0; i < ntimes; i++) {
+			try {
+				lock.lockInterruptibly();
+				counter.inc();
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} finally {
+				lock.unlock();
+			}
 		}
 	}
 }
